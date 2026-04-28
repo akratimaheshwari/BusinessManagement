5820fe409dbd31432e863fea2b4273798a391677
akratimaheshwari <akratimaheshwari634@gmail.in>
2026-04-28 12:34:41 +0530

    add logic file

---
+++ BusinessManagemnt/src/main/java/com/example/BusinessManagemnt/basiclogics/logic.java
@@ -0,0 +1,11 @@
+package com.example.BusinessManagemnt.basiclogics;
+
+public class logic {
+    public static double countTotal(double price,int quantity)
+    {
+        double res=price*quantity;
+        return res;
+    }
+}
+
+

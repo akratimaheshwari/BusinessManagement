0b74244ef2d71e9054b80b0426feaceeda02b282
akratimaheshwari <akratimaheshwari634@gmail.in>
2026-04-28 12:28:23 +0530

    project structure

---
+++ BusinessManagemnt/src/main/java/com/example/BusinessManagemnt/BusinessManagemntApplication.java
@@ -0,0 +1,13 @@
+package com.example.BusinessManagemnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessManagemntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessManagemntApplication.class, args);
+	}
+
+}
---
+++ BusinessManagemnt/src/test/java/com/example/BusinessManagemnt/BusinessManagemntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BusinessManagemnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagemntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

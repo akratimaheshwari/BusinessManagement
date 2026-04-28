d4e4528410189464b1fffa20040b3e4f24ba46f6
Harshitaa <harshitatyagi025@gmail.com>
2026-04-28 14:40:55 +0530

    Login Credientals

--- BusinessManagemnt/src/main/java/com/example/BusinessManagemnt/BusinessManagemntApplication.java
+++ BusinessManagemnt/src/main/java/com/example/BusinessManagemnt/BusinessManagemntApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BusinessManagemntApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BusinessManagemntApplication.class, args);
 	}
